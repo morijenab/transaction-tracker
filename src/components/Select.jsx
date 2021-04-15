8ed15fb0cafe47a40cb5c604c27de52c58d8cfec
Morteza <m.jenabzadeh@gmail.com>
2021-04-15 15:38:02 +0430

    Create Select component
    Select component its a Drop Down component

---
+++ src/components/Select.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Select({ label, options, name, onChange }) {
+  if (!options || options?.length < 1) return <></>;
+  return (
+    <div>
+      <label>{label}</label>
+      <select
+        name={name}
+        className="base-input"
+        onChange={(e) => onChange({ value: e.target.value, name })}
+      >
+        {options?.map?.((item) => (
+          <option value={item?.value} key={item?.id}>
+            {item?.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+Select.propTypes = {
+  label: PropTypes.string,
+  name: PropTypes.string,
+  options: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number,
+      value: PropTypes.string,
+      label: PropTypes.string,
+    })
+  ),
+};
+
+export default Select;
