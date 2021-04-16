189ae072ff05f3fc889d539b47b1c60cedba06cb
Morteza <m.jenabzadeh@gmail.com>
2021-04-16 14:28:12 +0430

    add new tests for the wallet

--- src/helpers/wallet.test.js
+++ src/helpers/wallet.test.js
@@ -1,4 +1,14 @@
-import { isTransactionValid } from "./wallet";
+import { isTransactionValid, aggregateWallet } from "./wallet";
+
+const mockRecords1 = [
+  { type: "income", amount: 100, note: "100 ", date: "2021-04-16", id: 0 },
+  { type: "income", amount: 200, note: "100 ", date: "2021-04-16", id: 1 },
+];
+
+const mockRecords2 = [
+  { type: "income", amount: 300, note: "100 ", date: "2021-04-16", id: 0 },
+  { type: "expense", amount: 100, note: "100 ", date: "2021-04-16", id: 1 },
+];
 
 describe("wallet", () => {
   test("should return false in missing argument conditions", () => {
@@ -15,5 +25,23 @@ describe("wallet", () => {
 
   test("should returns true when the expense is lower than wallet", () => {
     expect(isTransactionValid(200, "expense", 150)).toBe(true);
+  });
+});
+
+describe("aggregation", () => {
+  test("should return 0 in a case of missing props", () => {
+    expect(aggregateWallet()).toBe(0);
+  });
+
+  test("should returns 0 when the array is empty", () => {
+    expect(aggregateWallet([])).toEqual(0);
+  });
+
+  test("should returns 300 for mock data", () => {
+    expect(aggregateWallet(mockRecords1)).toEqual(300);
+  });
+
+  test("should returns 200 for mock data", () => {
+    expect(aggregateWallet(mockRecords2)).toEqual(200);
   });
 });
