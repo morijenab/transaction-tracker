e71983285edb3761edcea5f71aa0a0e6b41682fb
Morteza <m.jenabzadeh@gmail.com>
2021-04-16 10:53:33 +0430

    app updated with context provider

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import "./App.css";
 import TransactionForm from "./screens/TransactionsForm";
 import TransactionList from "./screens/TransactionList";
 import Wallet from "./screens/Wallet";
+import { WalletProvider } from "./context/walletProvider";
 function App() {
   return (
     <div className="App">
-      <Wallet />
-      <Router>
-        <Switch>
-          <Route path={["/transaction-entry/:id", "/transaction-entry"]}>
-            <TransactionForm />
-          </Route>
-          <Route path="/">
-            <TransactionList />
-          </Route>
-        </Switch>
-      </Router>
+      <WalletProvider>
+        <Wallet />
+        <Router>
+          <Switch>
+            <Route path={["/transaction-entry/:id", "/transaction-entry"]}>
+              <TransactionForm />
+            </Route>
+            <Route path="/">
+              <TransactionList />
+            </Route>
+          </Switch>
+        </Router>
+      </WalletProvider>
     </div>
   );
 }
