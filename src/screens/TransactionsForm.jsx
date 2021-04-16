519f408c42e33cc900824dfb16168e5408ec52c0
Morteza <m.jenabzadeh@gmail.com>
2021-04-16 09:11:19 +0430

    file name changed from trnasaction to transaction form

--- src/screens/TransactionsForm.jsx
+++ src/screens/TransactionsForm.jsx
@@ -1,20 +1,27 @@
 import React from "react";
+import { useHistory, useLocation, Link } from "react-router-dom";
 import NumberInput from "../components/NumberInput";
 import TextArea from "../components/TextArea";
 import DatePicker from "../components/DatePicker";
 import Select from "./../components/Select";
+import { saveData } from "../helpers/localStorage";
+import Wallet from "./Wallet";
 const PAYMENT_OPTIONS = [
   { value: "income", label: "Income", id: 1 },
   { value: "expense", label: "Expense", id: 2 },
 ];
 
-const Transaction = () => {
-  const [data, setDate] = React.useState({});
+const TransactionForm = () => {
+  const [data, setDate] = React.useState({ type: "income" });
+  const history = useHistory();
+  const { state = {} } = useLocation();
+  const { amount, note, date, type, id } = state;
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    localStorage.setItem("transaction", JSON.stringify(data));
-    console.log(data);
+    const dataBase = JSON.parse(localStorage.getItem("transactions")) ?? [];
+    saveData(dataBase, data, id);
+    history.push("/");
   };
 
   const handleChange = ({ value, name }) => {
@@ -23,6 +30,11 @@ const Transaction = () => {
 
   return (
     <div>
+      <Link to="/" className="back-home-btn">
+        {" "}
+        {"<-back"}
+      </Link>
+      <Wallet />
       <form onSubmit={handleSubmit}>
         <NumberInput
           label="Enter amount"
@@ -31,14 +43,28 @@ const Transaction = () => {
           min={1}
           required={true}
           onBlur={handleChange}
+          defaultValue={amount}
         />
-        <TextArea label="Note" name={"note"} onBlur={handleChange} />
-        <DatePicker label={"Date"} onBlur={handleChange} name={"date"} />
+        <TextArea
+          label="Note"
+          name={"note"}
+          onBlur={handleChange}
+          required={true}
+          defaultValue={note}
+        />
+        <DatePicker
+          label={"Date"}
+          onBlur={handleChange}
+          name={"date"}
+          required={true}
+          defaultValue={date}
+        />
         <Select
           label={"Category"}
           options={PAYMENT_OPTIONS}
           name={"type"}
           onChange={handleChange}
+          defaultValue={type}
         />
         <div className={"action-area"}>
           <button
@@ -57,4 +83,4 @@ const Transaction = () => {
   );
 };
 
-export default Transaction;
+export default TransactionForm;
