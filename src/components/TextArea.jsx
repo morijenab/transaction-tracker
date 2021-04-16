b6a28175a1b19bb8b51a62e3a7810cd097a81d58
Morteza <m.jenabzadeh@gmail.com>
2021-04-16 08:58:35 +0430

    add defaultvalue and required to text area

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-function TextArea({ label, name, onBlur }) {
+function TextArea({ label, name, onBlur, required, defaultValue }) {
   return (
     <div>
       <label>{label}</label>
       <textarea
+        required={required}
         className="base-input"
         name={name}
         onBlur={(e) => onBlur({ value: e.target.value, name })}
+        defaultValue={defaultValue}
       />
     </div>
   );
