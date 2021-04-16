9e75c1f80c84639e5fd7546bb98aea895d098548
Morteza <m.jenabzadeh@gmail.com>
2021-04-16 08:57:08 +0430

    add defaultvalue and required to date picker

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function DatePicker({ label, onBlur, name }) {
+function DatePicker({ label, onBlur, name, required, defaultValue }) {
   return (
     <div>
       <label>{label}</label>
       <input
+        required={required}
         type="date"
         className="base-input"
-        onBlur={(e) => onBlur({ value: e.target.valueAsDate, name })}
+        onBlur={(e) => onBlur({ value: e.target.value, name })}
         name={name}
+        defaultValue={defaultValue}
       />
     </div>
   );
